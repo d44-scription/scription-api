66adc34ecc13fbdaba71754b0849e4149c2ecedd
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-10-28 15:36:10 +0000

    Created noteables with link table to notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@
 
 class Note < ApplicationRecord
   belongs_to :notebook
+  has_and_belongs_to_many :notables
 
   validates :notebook, presence: true
   validates :content, presence: true, length: { in: 5..500 }
