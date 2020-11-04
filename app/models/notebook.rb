5aa37a3ff1da8da0a3e365260ba9d87cd31f4909
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-04 14:35:44 +0000

    Added dependent: destroy to models, added specs to confirm notebooks can be deleted

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Notebook < ApplicationRecord
-  has_many :notes
-  has_many :notables
-  has_many :items
-  has_many :characters
-  has_many :locations
+  has_many :notes, dependent: :destroy
+  has_many :notables, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :characters, dependent: :destroy
+  has_many :locations, dependent: :destroy
 
   validates :name, presence: true
 end
