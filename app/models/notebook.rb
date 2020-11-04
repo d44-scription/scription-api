f724727701e920bddf7329faf537a0aa4461db76
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-04 14:38:21 +0000

    Use of `with_option` to neaten code

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Notebook < ApplicationRecord
-  has_many :notes, dependent: :destroy
-  has_many :notables, dependent: :destroy
-  has_many :items, dependent: :destroy
-  has_many :characters, dependent: :destroy
-  has_many :locations, dependent: :destroy
+  with_options dependent: :destroy do |model|
+    model.has_many :notes
+    model.has_many :notables
+    model.has_many :items
+    model.has_many :characters
+    model.has_many :locations
+  end
 
   validates :name, presence: true
 end
