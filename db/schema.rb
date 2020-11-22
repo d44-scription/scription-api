84a7209a0ce49956cb1a624baef93bc80087aca1
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2020-11-22 17:08:15 +0000

    Added new field to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_153820) do
+ActiveRecord::Schema.define(version: 2020_11_22_164310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_153820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "summary"
+    t.integer "order_index"
   end
 
   create_table "notes", force: :cascade do |t|
