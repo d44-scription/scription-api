9fcfa284e7b05b425b3bb02bb59bf7fe8ae454fb
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-01-19 12:19:52 +0000

    Added specs to cover querying characters

--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -30,5 +30,62 @@ RSpec.describe '/api/v1/notebooks/:id/characters', type: :request do
       expect(response.body).not_to include(item.name)
       expect(response.body).not_to include(location.name)
     end
+
+    describe 'when searching' do
+      let!(:character_3) { FactoryBot.create(:notable, :character, notebook: notebook_1, name: "Different Character") }
+      let!(:character_4) { FactoryBot.create(:notable, :character, notebook: notebook_1, name: "Another Character") }
+
+      it 'returns a subset of characters matching the search query' do
+        get api_v1_notebook_characters_url(notebook_1, q: "Different"), headers: valid_headers, as: :json
+
+        expect(response).to be_successful
+        expect(response.body).not_to include(character_1.name)
+        expect(response.body).to include(character_3.name)
+        expect(response.body).not_to include(character_4.name)
+
+        expect(response.body).not_to include(character_2.name)
+        expect(response.body).not_to include(item.name)
+        expect(response.body).not_to include(location.name)
+      end
+
+      it 'returns an empty array when no characters match the query' do
+        get api_v1_notebook_characters_url(notebook_1, q: "TEST"), headers: valid_headers, as: :json
+
+        expect(response).to be_successful
+        expect(response.body).not_to include(character_1.name)
+        expect(response.body).not_to include(character_3.name)
+        expect(response.body).not_to include(character_4.name)
+
+        expect(response.body).not_to include(character_2.name)
+        expect(response.body).not_to include(item.name)
+        expect(response.body).not_to include(location.name)
+      end
+
+      it 'returns all characters when all match the query' do
+        get api_v1_notebook_characters_url(notebook_1, q: "Character"), headers: valid_headers, as: :json
+
+        expect(response).to be_successful
+        expect(response.body).to include(character_1.name)
+        expect(response.body).to include(character_3.name)
+        expect(response.body).to include(character_4.name)
+
+        expect(response.body).not_to include(character_2.name)
+        expect(response.body).not_to include(item.name)
+        expect(response.body).not_to include(location.name)
+      end
+
+      it 'ignores case when searching' do
+        get api_v1_notebook_characters_url(notebook_1, q: "aNoThEr"), headers: valid_headers, as: :json
+
+        expect(response).to be_successful
+        expect(response.body).not_to include(character_1.name)
+        expect(response.body).not_to include(character_3.name)
+        expect(response.body).to include(character_4.name)
+
+        expect(response.body).not_to include(character_2.name)
+        expect(response.body).not_to include(item.name)
+        expect(response.body).not_to include(location.name)
+      end
+    end
   end
 end
