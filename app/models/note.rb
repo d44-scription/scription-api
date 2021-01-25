95ffff321b5e8b4926dc05bef2f8887a93459d9d
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-01-25 16:24:45 +0000

    [WIP] Adding before hook and validator to connect notes to notables

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,6 +4,46 @@ class Note < ApplicationRecord
   belongs_to :notebook
   has_and_belongs_to_many :notables
 
+  before_validation :clear_notables
+  before_validation :link_characters
+
+  validate :permitted_notables
   validates :notebook, presence: true
   validates :content, presence: true, length: { in: 5..500 }
+
+  # RegEx for the format of a notable:
+  # Trigger[Name](Trigger Id)
+  CHARACTER_REGEX = /@\[[^@]*\]\(@\d\)/
+
+  private
+
+  def permitted_notables
+    errors.add(:notables, "must be from this notebook") if notables.any? do |n|
+      n.notebook != notebook
+    end
+  end
+
+  def clear_notables
+    notables = []
+  end
+
+  def link_characters
+    # Find all matching instances of the regex
+    character_codes = content.scan(CHARACTER_REGEX)
+
+    character_codes.each do |c|
+      # Remove the text surrounding the id
+      id = c.gsub(/@\[[^@]*\]\(@/, '').delete(')')
+
+      # Find the character
+      character = notebook.characters.find_by(id: id)
+
+      if character
+        # Link the character to the notebook
+        notables << character
+      else
+        errors.add(:characters, "must be from this notebook")
+      end
+    end
+  end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -52,4 +52,53 @@ RSpec.describe Note, type: :model do
     expect(note.errors.full_messages).to include('Content is too long (maximum is 500 characters)')
     expect(note).not_to be_valid
   end
+
+  describe 'linking to notables' do
+    let!(:notebook_2) { FactoryBot.create(:notebook) }
+
+    let!(:character_1) { FactoryBot.create(:notable, :character, notebook: notebook) }
+    let!(:character_2) { FactoryBot.create(:notable, :character, notebook: notebook) }
+    let!(:character_3) { FactoryBot.create(:notable, :character, notebook: notebook_2) }
+
+    let!(:character_1_content) { "@[#{character_1.name}](@#{character_1.id})"}
+    let!(:character_2_content) { "@[#{character_2.name}](@#{character_2.id})"}
+    let!(:character_3_content) { "@[#{character_3.name}](@#{character_3.id})"}
+
+    it 'correctly links to characters' do
+      note.content = "This is a note for #{character_1_content} and (#{character_2_content})"
+
+      note.save
+
+      expect(note).to be_valid
+
+      raise character_1.notes.inspect
+      expect(note.notables.count).to eql 2
+
+      expect(note.notables.pluck(:id)).to include(character_1.id)
+      expect(note.notables.pluck(:name)).to include(character_1.name)
+
+      expect(note.notables.pluck(:id)).to include(character_2.id)
+      expect(note.notables.pluck(:name)).to include(character_2.name)
+
+      expect(note.notables.pluck(:id)).not_to include(character_3.id)
+      expect(note.notables.pluck(:name)).not_to include(character_3.name)
+    end
+
+    it 'returns an error when linking a character from a different notebook' do
+      note.content = "This is a note for #{character_3_content}"
+
+      expect(note).not_to be_valid
+      expect(note.notables.count).to eql 0
+      expect(note.errors.full_messages).to include("Characters must be from this notebook")
+
+      expect(note.notables.pluck(:id)).not_to include(character_1.id)
+      expect(note.notables.pluck(:name)).not_to include(character_1.name)
+
+      expect(note.notables.pluck(:id)).not_to include(character_2.id)
+      expect(note.notables.pluck(:name)).not_to include(character_2.name)
+
+      expect(note.notables.pluck(:id)).not_to include(character_3.id)
+      expect(note.notables.pluck(:name)).not_to include(character_3.name)
+    end
+  end
 end
