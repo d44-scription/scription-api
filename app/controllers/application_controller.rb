2c013fe9fa0bc6b29afa5c965bb3c452eb0a78d4
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-08 16:26:12 +0000

    Added authorisation action to app controller to secure endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authorise_request
+
+  private
+
+  def authorise_request
+    AuthorisationService.new(request.headers).authenticate_request!
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
 end
