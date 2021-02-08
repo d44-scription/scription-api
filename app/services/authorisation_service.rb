af50a3c44e0b1d9e83e89e9a811615c6c2528294
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-08 16:23:07 +0000

    Added standard authorisation service

---
+++ app/services/authorisation_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class AuthorisationService
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def authenticate_request!
+    verify_token
+  end
+
+  private
+
+  def http_token
+    @headers['Authorization'].split.last if @headers['Authorization'].present?
+  end
+
+  def verify_token
+    JsonWebToken.verify(http_token)
+  end
+end
