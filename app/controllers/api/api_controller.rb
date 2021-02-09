0fdaaec4a8d8d17915caae27001089f6565478eb
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-09 15:32:01 +0000

    Initial Postman tests working usccessfully

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,3 +1,3 @@
-class ApiController < ApplicationController
+class Api::ApiController < ApplicationController
   before_action :authenticate_user!
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   respond_to :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
