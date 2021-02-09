b033110d21c87cc49ea47faab668cb4a946179a8
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-09 10:35:20 +0000

    Added sessions controller override to respond to failed logins appropriately

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      devise_for :users
+      devise_for :users, controllers: { sessions: :sessions },
+                        path_names: { sign_in: :login }
 
       resources :notebooks do
         resources :notes
