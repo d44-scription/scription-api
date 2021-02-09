a1456fdff9ae80bde1e6106f56bcdbaa8016da99
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-09 14:05:16 +0000

    Moved authenticate action into separate api contorller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
+  respond_to :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
-  before_action :authenticate_user!
 
   private
 
