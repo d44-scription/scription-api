d26f52bfdeeceb54318939b6b5ad082e71b1c898
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-09 10:26:29 +0000

    Added generate_jwt model to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :display_name, length: { maximum: 25 }
+
+  def generate_jwt
+    JWT.encode({ id: id,
+                exp: 60.days.from_now.to_i },
+              Rails.application.secrets.secret_key_base)
+  end
 end
