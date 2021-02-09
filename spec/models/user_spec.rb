33097ce13adbeda2a2217117921d7d9e7449f314
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-09 16:59:54 +0000

    Added model tests for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,43 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    it 'is valid without an email' do
+    it 'is not valid without an email' do
       user.email = nil
 
       expect(user).to have(1).errors_on(:email)
       expect(user).to have(0).errors_on(:password)
       expect(user).to have(0).errors_on(:password_confirmation)
+      expect(user).to have(0).errors_on(:display_name)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid when email fails regex' do
+      user.email = 'Test'
+
+      expect(user).to have(1).errors_on(:email)
+      expect(user).to have(0).errors_on(:password)
+      expect(user).to have(0).errors_on(:password_confirmation)
+      expect(user).to have(0).errors_on(:display_name)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a password' do
+      user.password = nil
+
+      expect(user).to have(0).errors_on(:email)
+      expect(user).to have(1).errors_on(:password)
+      expect(user).to have(0).errors_on(:password_confirmation)
+      expect(user).to have(0).errors_on(:display_name)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid when password and password confirmation do not match' do
+      user.password = 'Test Password'
+      user.password_confirmation = 'Test Password 2'
+
+      expect(user).to have(0).errors_on(:email)
+      expect(user).to have(0).errors_on(:password)
+      expect(user).to have(1).errors_on(:password_confirmation)
       expect(user).to have(0).errors_on(:display_name)
       expect(user).not_to be_valid
     end
