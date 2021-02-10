512a4dac939fa2e18cdb5597ef0fdfb25e17215a
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-10 10:45:23 +0000

    Fixed syntax for symbol list :| :| :man_facepalming:

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show, update]
+      resources :users, only: %i[show update]
       resources :notebooks do
         resources :notes
         resources :items, only: %i[index]
