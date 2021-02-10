2471e665429efba1e969ccae1a48d20983b176d9
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-10 12:01:48 +0000

    Minor patches to user controller to standardise it

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,9 @@
 module Api
   module V1
     class UsersController < ApiController
-      def show
-      end
-
       def update
         if current_user.update(user_params)
-          render :show
+          render :show, status: :ok
         else
           render json: current_user.errors.full_messages, status: :unprocessable_entity
         end
