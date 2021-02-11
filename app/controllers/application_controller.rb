ef9482b67afec94c0644ffe6d6a8e0d853988cd1
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-11 12:41:58 +0000

    Moved raiseg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,11 @@ class ApplicationController < ActionController::API
   def authenticate_user
     render json: { errors: ['Not Authenticated'] }, status: :unauthorized && return unless request.headers['Authorization'].present?
 
-    raise 'here'
-
     authenticate_or_request_with_http_token do |token|
       jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
 
       @current_user_id = jwt_payload['id']
+          raise 'here'
     rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
       render json: { errors: ['Not Authenticated'] }, status: :unauthorized
     end
