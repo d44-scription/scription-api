3580d11e076d7b4ebb1bee085e2e45e69b516bee
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-14 17:20:55 +0000

    Added config to allow cookie use

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3001', 'scription-app-staging.herokuapp.com', 'scription-app.herokuapp.com'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    headers: :any,
+    credentials: true,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
