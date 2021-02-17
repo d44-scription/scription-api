78d39f6d0fcb0c07e55187a655d1e4415630fdb0
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-02-17 17:35:24 +0000

    Using hardcoded strings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', /^scription-ap.*\.herokuapp\.com\/$/
+    origins 'localhost:3001', 'scription-app-staging.herokuapp.com', 'scription-app.herokuapp.com'
 
     resource '*',
     headers: :any,
